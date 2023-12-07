5d1fa2116ef298d19b087ff0b1335afc5629a60f
daibv <152725726+daibv@users.noreply.github.com>
2023-12-07 23:07:58 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;//khai bao bien kieu float cho chieu rong width
+            float height;//khai bao bien kieu float cho chieu cao height
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());//Sử dụng Console.ReadLine() để lấy dữ liệu nhập vào từ người dùng
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());//Sử dụng Console.ReadLine() để lấy dữ liệu nhập vào từ người dùng
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+}
